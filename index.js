200b19134964f637ad5cf52637212fd321d7eba5
Tushar Garg <garg31@uwindsor.ca>
2020-05-14 15:18:09 -0400

    Adding basics winners plag image

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ var p2 = player_2+" won";
 
 //deciding the winner.
 if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 wins";
+    document.querySelector("h1").innerHTML = "🚩 Player 1 wins!";
 }
 else if(randomNumber1 < randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 2 wins";
+    document.querySelector("h1").innerHTML = "Player 2 wins! 🚩";
 }
 else{
     document.querySelector("h1").innerHTML = "Draw";
